33593475b4b154c350be6e1f3436d112c4d89afd
YANG <atyzyang@gmail.com>
2021-01-16 01:11:01 +0800

    commit

--- api/api.js
+++ api/api.js
@@ -1,38 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class Api {
-
+    
     request = async (url, method, data) => {
-        let baseUrl = 'http://192.168.0.197/HospitalQ/public/api/';
-        
         try {
             let userToken = 'Bearer ' + await AsyncStorage.getItem('userToken');
+            let baseUrl = 'http://192.168.0.197/HospitalQ/public/api/';
+            let second_argument = {
+                method: method,
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': userToken
+                }
+            };
             if (method == 'POST') {
-                let response = await fetch(baseUrl + url, {
-                    method: method,
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': userToken
-                    },
-                    body: JSON.stringify(data)
-
-                });
-                let json = await response.json();
-                return json;
-            } else {
-                let response = await fetch(baseUrl + url, {
-                    method: method,
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': userToken
-                    }
-                });
-                let json = await response.json();
-                return json;
+                second_argument.body = data;
             }
-
+            let response = await fetch(baseUrl + url, second_argument);
+            let json = await response.json();
+            return json;
 
         } catch (error) {
             console.error(error);
