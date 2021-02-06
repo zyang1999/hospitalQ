f053a6ff6a5398e3c78075641bbd2b8cf243356c
YANG <atyzyang@gmail.com>
2021-02-06 12:49:32 +0800

    commit

--- App.js
+++ App.js
@@ -1,6 +1,49 @@
 import React from 'react';
 import Authentication from './services/Authentication';
+import messaging from '@react-native-firebase/messaging';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import { Platform } from 'react-native';
 
 export default function App() {
-  return(<Authentication/>);
+  async function saveTokenToDatabase(token) {
+    // Assume user is already signed in
+    const userId = auth().currentUser.uid;
+
+    // Add the token to the users datastore
+    await firestore()
+      .collection('users')
+      .doc(userId)
+      .update({
+        tokens: firestore.FieldValue.arrayUnion(token),
+      });
+  }
+
+  useEffect(() => {
+    // Get the device token
+    messaging()
+      .getToken()
+      .then(token => {
+        return saveTokenToDatabase(token);
+      });
+
+    // If using other push notification providers (ie Amazon SNS, etc)
+    // you may need to get the APNs token instead for iOS:
+    // if(Platform.OS == 'ios') { messaging().getAPNSToken().then(token => { return saveTokenToDatabase(token); }); }
+
+    // Listen to whether the token changes
+    return messaging().onTokenRefresh(token => {
+      saveTokenToDatabase(token);
+    });
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, [])
+
+  return (<Authentication />);
 }
